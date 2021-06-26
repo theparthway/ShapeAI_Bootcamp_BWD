a3e6f6a751cfdb4a5610d1588d33206a77d37863
theparthway <64493618+theparthway@users.noreply.github.com>
2021-06-27 00:06:45 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright Parth Shah @ 2021</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web dev bootcamp</h1>
+      <p>It is good one</p>
+    </div>
+  );
+}
+
+export default Note;
